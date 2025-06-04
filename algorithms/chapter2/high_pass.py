4cf5ebdd8a455fd14dc14f501e8753f3aee8f2ac
million-dollar-dev <hoangtuan1304@hotmail.com>
2025-06-04 21:47:43 +0700

    high pass

--- algorithms/chapter2/high_pass.py
+++ algorithms/chapter2/high_pass.py
@@ -7,25 +7,26 @@ def create_gaussian_filter(shape, d0):
     m, n = shape
     for u in range(m):
         for v in range(n):
-            d = np.sqrt((u - m/2) ** 2 + (v - n/2) ** 2)
-            h[u, v] = np.exp(-d * d / (2 * d0 * d0))
+            d = np.sqrt((u - m/2) ** 2 + (v - n / 2) ** 2)
+            h[u, v] = 1 - np.exp((-d * d) / 2 * d0 * d0)
     return h
 
-def low_pass_filter(gray, d):
-    #B1
+def high_pass_filter(gray, d):
     f = np.fft.fft2(gray)
     f = np.fft.fftshift(f)
-    show_spectrum(f, title="Spectrum")
-    #B2
+    show_spectrum(f, "F(u,v) - Before Filtering")
+
     h = create_gaussian_filter(gray.shape, d)
-    #B3
+
     g = f * h
-    show_spectrum(g, title="Spectrum")
-    #B4
+    show_spectrum(g, "F(u,v) - After Filtering")
+
     g = np.fft.ifftshift(g)
     g = np.fft.ifft2(g)
+
     out = np.abs(g)
     return np.clip(out, 0, 255).astype(np.uint8)
+
 
 def show_spectrum(freq, title="Spectrum"):
     magnitude = 20 * np.log(np.abs(freq) + 1)
@@ -38,11 +39,10 @@ def show_spectrum(freq, title="Spectrum"):
 file = 'E:\\Workspace\\LEARN\\NLU\\DIP\\images\\lenna.jpg'
 
 img = cv.imread(file, 0)
-low_pass_img = low_pass_filter(img, 10)
+high_pass_img = high_pass_filter(img, 150)
 
 cv.imshow('default', img)
-cv.imshow('low_pass_img', low_pass_img)
+cv.imshow('high_pass_img', high_pass_img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
-
